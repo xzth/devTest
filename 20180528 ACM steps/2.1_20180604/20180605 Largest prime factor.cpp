2b72b4e1d5dd98a64440a41a4ccbdf90ececb664
xzth <1419393745@qq.com>
2018-06-08 21:59:06 +0800

    Lab

--- 20180528 ACM steps/2.1_20180604/20180605 Largest prime factor.cpp
+++ 20180528 ACM steps/2.1_20180604/20180605 Largest prime factor.cpp
@@ -49,120 +49,31 @@
 //ÍþÊ¿¼É
 
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
 
 using namespace std;
 
-class PrimeList{
-	int *plist;
-	int length;
-	
-	void expand(){
-		int *newlist = new int[2 * length];
-		memset(newlist, 0, 2 * length * sizeof(int));
-		memcpy(newlist, plist, length * sizeof(int));
-		delete[] plist;
-		plist = newlist;
-	}
-	
-	public:
-		PrimeList(){
-			length = 100;
-			plist = new int[100];
-			memset(plist, 0, 100 * sizeof(int));
-			plist[0] = 1;
-			plist[1] = 2;
-			plist[2] = 3;
-		}
-		~PrimeList(){
-			delete[] plist;
-		}
-		
-		bool isPrime(int n){
-			
-			if(n == 2){
-				return true;
-			}
-			if(n < 2){
-				return false;
-			}
-			if((n&1) == 0){
-				return false;
-			}
-			
-			int roof = (int)sqrt((double)n);
-			int i = 2;
-			while(plist[i] <= roof){
-				if(n%plist[i] == 0){
-					return false;
-				}
-				++i;
-				if(i >= length){
-					expand();
-				}
-				if(plist[i] == 0){
-					int j;
-					for(j = plist[i-1]+2; !isPrime(j); j+=2){}
-					plist[i] = j;
-				}
-			}
-			return true;
-		}
-		
-		int getPrimePos(int n){
-			
-			if(n == 1){
-				return 0;
-			}
-			
-			int roof = (int)sqrt((double)n);
-			int i = 1, max = 0;
-			while(plist[i] <= roof){
-				if(n%plist[i] == 0){
-					max = getPrimePos(n / plist[i]);
-					if(i > max){
-						max = i;
-					}
-					return max;
-				}
-				++i;
-				if(i >= length){
-					expand();
-				}
-				if(plist[i] == 0){
-					int j;
-					for(j = plist[i-1]+2; !isPrime(j); j+=2){}
-					plist[i] = j;
-				}
-			}
-			
-			while(plist[i] != n){
-				++i;
-				
-				if(i >= length){
-					expand();
-				}
-				if(plist[i] == 0){
-					int j;
-					for(j = plist[i-1]+2; !isPrime(j); j+=2){}
-					plist[i] = j;
-				}
-			}
-			return i;
-		}
-};
+#define len 1000001
 
 int main(){
 	
-	int n;
+	int n, pos = 1, list[len];
 	
-	PrimeList list;
+	memset(list, 0, len * sizeof(int));
+	
+	for(int i = 2; i < len; ++i){
+		if(list[i] == 0){
+			for(int j = i; j < len; j += i){
+				list[j] = pos;
+			}
+			++pos;
+		}
+	}
 	
 	while(scanf("%d", &n) != EOF){
-		printf("%d\n", list.getPrimePos(n));
-//		printf("%d\n", list.isPrime(n));
+		printf("%d\n", list[n]);
 	}
 	
 	return 0;
+	
 }
